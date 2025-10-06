922c366324e0e12fe4494b56079e123274584c0a
AmidokS <aalimhulov@gmail.com>
2025-10-06 18:49:52 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGkQ3LeMnRecqpevmVHq8jPUmCXeHAuA",
+  apiKey: "AIzaSyCGN93LsNnRGcqGpesVWAg8jP0m6XsQAuA",
   authDomain: "budget-ami.firebaseapp.com",
   databaseURL: "https://budget-ami-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "budget-ami",
   storageBucket: "budget-ami.firebasestorage.app",
   messagingSenderId: "976854941281",
-  appId: "1:976854941281:web:f40ea1033cf52d236af420"
+  appId: "1:976854941281:web:f40e81033cf52d236af420"
 };
 
 const app = initializeApp(firebaseConfig);
